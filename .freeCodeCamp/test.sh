affef21432ca9d5bd844b776f86f59cb1ad081ab
shrihdar <shridharhksu143@gmail.com>
2025-08-11 14:42:23 -0400

    Save progress: 1150.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -2,6 +2,5 @@
 #Program to tell a persons fortune
 echo -e "\n~~ Fortune Teller ~~\n"
 RESPONSES=("Yes" "No" "Maybe" "Outlook good" "Don't count on it" "Ask again later")
-N=$(( RANDOM % 6 ))
-echo ${RESPONSES[5]}
-echo $N
+N=$(( RANDOM %1 ))
+echo ${RESPONSES[$N]}

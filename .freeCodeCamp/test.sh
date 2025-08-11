36b001366220bb69b2dd4a917561d5d98c721b6a
shrihdar <shridharhksu143@gmail.com>
2025-08-11 13:00:37 -0400

    Save progress: 340.1

--- .freeCodeCamp/test.sh
+++ .freeCodeCamp/test.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+echo -e "\n~~ Questionnaire ~~\n"
 QUESTION1="What's your name?"
 QUESTION2="Where are you from?"
+QUESTION3="What's your favorite coding website?"
 echo $QUESTION1
 read NAME
 echo $QUESTION2
 read LOCATION
-echo Hello $NAME.
+echo Hello $NAME from $LOCATION.
 
 
-echo $LOCATION
+echo $QUESTION3

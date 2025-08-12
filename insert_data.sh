b13f68b9869f953e51527a776ae23fd3e9e9f13c
shrihdar <shridharhksu143@gmail.com>
2025-08-12 05:12:59 -0400

    Save progress: 1060.1

--- insert_data.sh
+++ insert_data.sh
@@ -55,5 +55,6 @@ do
   MAJOR_ID='null'
   fi
   #insert student
+  INSERT_STUDENT_RESULT=$($PSQL "INSERT INTO students(first_name,last_name,major_id,gpa) VALUES('$FIRST','$LAST',$MAJOR_ID,$GPA)")
   fi
 done

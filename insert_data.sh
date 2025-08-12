3d44785b08719caebc30bb3d9bfe2e02a2c48ad6
shrihdar <shridharhksu143@gmail.com>
2025-08-12 05:08:13 -0400

    Save progress: 1049.1

--- insert_data.sh
+++ insert_data.sh
@@ -48,12 +48,12 @@ do
   then
   #get major_id
   MAJOR_ID=$($PSQL "SELECT major_id FROM majors WHERE major='$MAJOR'")
-  echo $MAJOR_ID
   #if not found
   if [[ -z $MAJOR_ID ]]
   then
   #set to null
   MAJOR_ID='null'
+   echo $MAJOR_ID
   fi
   #insert student
   fi

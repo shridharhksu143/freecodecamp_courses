b8d2ddb9e4b56aa1b34683f6225f53dabf35192a
shrihdar <shridharhksu143@gmail.com>
2025-08-15 13:58:59 -0400

    Save progress: 1572.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -98,6 +98,7 @@ else
     MAIN_MENU "You do not have any bikes rented."
   else
     # display rented bikes
+    echo -e "\nHere are your rentals:"
     # ask for bike to return
     # if not a number
     # send to main menu

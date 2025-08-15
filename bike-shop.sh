d75fee72f381b899fae8074a8861ce949b9a4d2f
shrihdar <shridharhksu143@gmail.com>
2025-08-15 12:35:06 -0400

    Save progress: 1440.1

--- bike-shop.sh
+++ bike-shop.sh
@@ -71,7 +71,7 @@ else
       BIKE_INFO=$($PSQL "SELECT size,type FROM bikes WHERE bike_id=$BIKE_ID_TO_RENT;")
       BIKE_INFO_FORMATTED=$(echo $BIKE_INFO | sed 's/ |/"/')
       # send to main menu
-      MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $CUSTOMER_NAME."
+      MAIN_MENU "I have put you down for the $BIKE_INFO_FORMATTED Bike, $(echo $CUSTOMER_NAME | sed -r 's/^ *| *$//g')."
     fi
   fi
 fi

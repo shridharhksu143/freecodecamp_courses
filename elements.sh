3b0076f47478e198b2eb3caf37dff6e3901074d2
shrihdar <shridharhksu143@gmail.com>
2025-08-17 09:06:47 -0400

    refactor: if and while loop for filter and display

--- elements.sh
+++ elements.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
-PSQL="psql --username=freecodecamp --dbname=peridoic_table -t --no-align -c"
+PSQL="psql --username=freecodecamp --dbname=periodic_table -t --no-align -c"
 echo Please provide an element as an argument.
-read arg
-if [[
+read ARG
+if [[ $ARG =~ ^[0-9]+$ ]]
+then
+  PROPERTIES=$($PSQL "SELECT type,atomic_mass,melting_point_celsius,boiling_point_celsius,name,symbol FROM properties INNER JOIN elements USING(atomic_number) WHERE atomic_number=$ARG;")
+  echo "$PROPERTIES" | while IFS="|" read TYPE MASS MELTING BOILING NAME SYMBOL
+  do
+    echo -e "\nThe element with atomic number $ARG is $NAME ($SYMBOL).It's a $TYPE,with a mass of $MASS amu.$NAME has a melting point of $MELTING celsius and a boiling point $BOILING celsius."
+  done
+fi

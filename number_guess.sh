a37c60dc0d56495b2644a992ac44c81af0c280e7
shrihdar <shridharhksu143@gmail.com>
2025-08-18 12:28:40 -0400

    feat: user_name_check variable added to check username availabality in users table

--- number_guess.sh
+++ number_guess.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
+#create a random number generator
 SECRET_NUMBER=$(( RANDOM % 1000 + 1 ))
+
+# connect to database
 PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
-DATA=$($PSQL "SELECT * FROM users;")
-echo $DATA
+
+#prompt user for requesting user name and read it
+echo "Enter your username:"
+read USER_NAME
+
+#create a variable to check availabality of username in users table
+USER_NAME_CHECK=$($PSQL "SELECT name FROM users WHERE name = '$USER_NAME';")

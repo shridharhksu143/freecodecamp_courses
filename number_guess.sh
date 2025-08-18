b8b71421a2c4382524bb5d5db26b096da119ef6d
shrihdar <shridharhksu143@gmail.com>
2025-08-18 12:49:11 -0400

    feat: loop added to validate user name

--- number_guess.sh
+++ number_guess.sh
@@ -7,7 +7,26 @@ PSQL="psql --username=freecodecamp --dbname=number_guess -t --no-align -c"
 
 #prompt user for requesting user name and read it
 echo "Enter your username:"
-read USER_NAME
 
 #create a variable to check availabality of username in users table
 USER_NAME_CHECK=$($PSQL "SELECT name FROM users WHERE name = '$USER_NAME';")
+
+#verify valid username
+while ! [[ $USER_NAME =~ ^[A-Za-z]+$ ]]
+do
+  read USER_NAME
+  if [[ $USER_NAME =~ ^[A-Za-z]+$ ]]
+  then
+    exit
+  else
+    echo "Please Enter a valid username"
+    continue
+  fi
+  
+done
+# if invalid username exit
+#if valid name proceed
+# create a if loop to check user availabality 
+#if available show welcome message
+#if not availabe show available show welcome message
+# insert into users table
